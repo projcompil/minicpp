97585a0299d455546d65232591b692d7c11a1b6f
projcompil <nissim.zerbib@ens.fr>
2013-12-24 12:18:14 +0100

    Ajout des arbres de syntaxe abstraites typés. Il faut maintenant coder les fonctions qui transformeront les arbres de ast.mli en arbre de typing.ml.

--- typing.ml
+++ typing.ml
@@ -13,87 +13,7 @@ type typ =
     | Tclass of string
 
 
-(*type targ = TArg of typedef * var
-
-type tqident =
-  | TQident of string
-  | TDouble of string * string
-
-type tqvar =
-  | TQvar of tqident
-  | TQpo of tqvar
-  | TQad of tqvar
-*)
-type tproto =
-  | TProtovide
-(*  | TPlong of typedef * tqvar * (arg list)
-  | TPshort of string * (arg list)
-  | TPdouble of string * string * (arg list)
-i*)
-(*
-type decl_v = Declv of typedef * (var list)
-i*)
-type texpr = { tdexpr:tdexpr ; typ:typ; }
-
-and tdexpr  =
-  | TEint of int
-  | TEthis
-  | TEbool of bool
-  | TEnull
-  | TEqident of qident
-  | TEpointeur of expr
-  | TEattr of expr * string
-  | TEsderef of expr * string
-  | TEassign of expr * expr
-  | TEfcall of expr * (expr list)
-  | TEnew of string * (expr list)
-  | TElincr of expr
-  | TEldecr of expr
-  | TErincr of expr
-  | TErdecr of expr
-  | TEland of expr
-  | TEnot of expr
-  | TEuminus of expr
-  | TEuplus of expr
-  | TEop of operateur * expr * expr
-  | TEpar of expr
-
-
-type texpr_str = { tdexpr_str:tdexpr_str ; typ:typ; }
-and tdexpr_str =
-  | TEsexpr of texpr
-  | TEstring of string
-
-
-type tinst = { tdinst:tdinst ; typ:typ; }
-
-and tdinst =
-  | TNothing
-  | TIexpr of expr
-(*  | TIdecls of typedef * var
-  | TIdecl of typedef * var * expr
-  | TAidecl of typedef * var * string * (expr list)
-  | TIf of expr * tinst
-  | TIfelse of expr * tinst * tinst
-  | TWhile of expr * tinst
-  | TFor of (expr list) * expr * (expr list) * tinst
-  | TAfor of (expr list) * (expr list) * tinst
-*)  | TIbloc of bloc
-  | TCout of expr_str list
-  | TReturn of expr
-  | TAreturn
-
-and tbloc = TBloc of tinst list
-
-and tdecl =
-(*  | TDv of tdecl_v
-  | TDc of tdecl_c*)
-  | TDb of tproto * tbloc
-
-type tfichier =
-  { tbincludeios : bool;
-    tdecls: tdecl list;
-  }
+type 'a atype = { v:'a ; typ:typ }
 
 
 
@@ -116,6 +36,115 @@ Hashtbl.add table_c "" "";;
 
 (* '*************************)
 
+
+
+type tsupers =  TSuper of  string list
+
+
+type tvar = tdvar atype
+and tdvar =
+  | Ident of string
+  | Po of tvar
+  | Ad of tvar
+
+
+type targ = TArg of typedef * var
+
+
+type tqident =
+  | TQident of string
+  | TDouble of string * string
+
+
+type tqvar =
+  | TQvar of tqident
+  | TQpo of tqvar
+  | TQad of tqvar
+
+
+type tproto =
+  | TPlong of typ * tqvar * (targ list)
+  | TPshort of string * (targ list)
+  | TPdouble of string * string * (targ list)
+
+
+type tdecl_v = TDeclv of typ * (tvar list)
+
+type tmembre =
+  | TMvar of tdecl_v
+  | TMvir of bool * tproto
+
+
+type tdecl_c =
+  | TClass of string *  tsupers * (tmembre list)
+  
+
+
+type texpr = tdexpr atype
+
+and tdexpr  =
+  | TEint of int
+  | TEthis
+  | TEbool of bool
+  | TEnull
+  | TEqident of tqident
+  | TEpointeur of texpr
+  | TEattr of texpr * string
+  | TEsderef of texpr * string
+  | TEassign of texpr * texpr
+  | TEfcall of texpr * (texpr list)
+  | TEnew of string * (texpr list)
+  | TElincr of texpr
+  | TEldecr of texpr
+  | TErincr of texpr
+  | TErdecr of texpr
+  | TEland of texpr
+  | TEnot of texpr
+  | TEuminus of texpr
+  | TEuplus of texpr
+  | TEop of operateur * texpr * texpr
+  | TEpar of texpr
+
+
+type texpr_str = tdexpr_str atype 
+and tdexpr_str =
+  | TEsexpr of texpr 
+  | TEstring of string
+
+
+
+type tinst =
+  | TNothing
+  | TIexpr of texpr
+  | TIdecls of typ * tvar
+  | TIdecl of typ * tvar * texpr
+  | TAidecl of typ * tvar * string * (texpr list)
+  | TIf of texpr * tinst
+  | TIfelse of texpr * tinst * tinst
+  | TWhile of texpr * tinst
+  | TFor of (texpr list) * texpr * (texpr list) * tinst
+  | TAfor of (texpr list) * (texpr list) * tinst
+  | TIbloc of tbloc
+  | TCout of texpr_str list
+  | TReturn of texpr
+  | TAreturn
+
+
+and tbloc = TBloc of tinst list
+
+
+and tdecl =
+  | TDv of tdecl_v
+  | TDc of tdecl_c
+  | TDb of tproto * tbloc
+
+
+and tfichier =
+  { tbincludeios : bool;
+    tdecls: tdecl list;
+  }
+
+(* ********************************** *)
 
 
 
