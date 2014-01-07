0477194b84aa0542b25ab05721a73ea7671b6cdb
projcompil <nissim.zerbib@ens.fr>
2014-01-07 22:25:51 +0100

    Ajout O.

--- compilateur.ml
+++ compilateur.ml
@@ -109,7 +109,7 @@ let rec code_expr lvl texpr = match texpr.c with
 				   end
 	| TEqident (TQident tid) -> (* il faut corriger les offset dans typage.ml *)
 			assert (tid.lvl <= lvl) ;
-			(move t1 fp) ++ (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ (if tid.byref then add a0 t0 oi tid.offset else lw a0 areg (tid.offset, t1)) ++ (if tid.byref then lw a0 areg (0, a0) else nop)
+			(move t1 fp) ++ (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ (if tid.byref then (*add a0 t0 oi tid.offset*)(lw a0 areg (tid.offset, t1)) else lw a0 areg (tid.offset, t1)) ++ (if tid.byref then lw a0 areg (0, a0) else nop)
 	| TEassign({ c = TEqident (TQident tid) ; typ = _  }, tf) -> 
 			(code_expr lvl tf) ++
 			(move t1 fp) ++ (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++
@@ -117,6 +117,12 @@ let rec code_expr lvl texpr = match texpr.c with
 			 else add t1 t1 oi tid.offset) ++
 			(sw a0 areg (0, t1))				
 (* pour l'instant uniquement les variables et non les membres d'objets *)
+
+    | TElincr ({ c = TEqident (TQident tid) ; typ = _ }) | TErincr ({ c = TEqident (TQident tid) ; typ = _ }) -> (code_expr lvl ({ c = TEqident (TQident tid) ; typ = Tint })) ++ add a0 a0 oi 1 ++ (move t1 fp) ++
+             (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ add t1 t1 oi tid.offset ++ (sw a0 areg (0, t1))
+                     | TEldecr ({ c = TEqident (TQident tid) ; typ = _ }) | TErdecr ({ c = TEqident (TQident tid) ; typ = _ }) -> (code_expr lvl ({ c = TEqident (TQident tid) ; typ = Tint })) ++ sub a0 a0 oi 1 ++ (move t1 fp) ++
+                              (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ add t1 t1 oi tid.offset ++ (sw a0 areg (0, t1))
+
 	| _ ->  ratec "Compilation de cette partie non encore implémentée.\n"
 
 
@@ -136,15 +142,24 @@ let code_cout_expr_str lvl (*env*) e = match e with
    		(move t1 fp) ++ 
    		iter (lvl - l) ++ (lw t1  (8,t1)); 
 *)
+(*
+	| TEqident (TQident tid) -> 
+			assert (tid.lvl <= lvl) ;
+			(move t1 fp) ++ (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ (if tid.byref then (*add a0 t0 oi tid.offset*)(lw a0 areg (tid.offset, t1)) else lw a0 areg (tid.offset, t1)) ++ (if tid.byref then lw a0 areg (0, a0) else nop)
+*)
+(*
+let calc_adresse q lvl = match q with
+    | TQident tid -> (move t1 fp) ++ (iter (lvl-tid.lvl) (lw t1 areg (8, t1))) ++ (if tid.byref then add a0 t0 oi tid.offset(*(lw a0 areg (tid.offset, t1)) *) else lw a0 areg (tid.offset, t1)) ++ (if tid.byref then lw a0 areg (0, a0) else nop)
 
-
-
+    | _ -> failwith "Erreur 0000\n"
+    *)
 let rec code_inst lvl ti = match ti with
 	| TNothing -> nopp
   	| TIexpr te -> { text = (code_expr lvl te) ; data = nop }
     | TIdecl (tt, tv) -> { text = sub sp sp oi (size_type ((extract_tvar tv).typ )); data = nop} (*Othmane.*)
-    | TIdeclinit (tt, tv, te) -> { text = sub sp sp oi (size_type ((extract_tvar tv).typ )) ++ (code_expr lvl te) ++ (move t1 fp) ++ (add t1 t1 oi (extract_tvar tv).offset) ++
-            (sw a0 areg (0, t1)) ; data = nop} 
+    | TIdeclinit (tt, tv, te) -> let tid = extract_tvar tv in { text = sub sp sp oi (size_type (tid.typ )) ++ (code_expr lvl te) ++ (move t1 fp) ++ (add t1 t1 oi tid.offset) ++
+            (sw a0 areg (0, t1)) ; data = nop}
+            (* problème ici pour les références *) 
             (*{text = (code_expr lvl te) ++ (push a0)  ; data = nop}*)
   	| TIdeclobj (tt, tv, s, tl, ni) -> ratec "" 
   	(*| TIf (te, ti) -> ratec ""*)
